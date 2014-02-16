ae03d135a1c310753278d303fc7266e0de074e8b
oleg <oleg@srv1.example.com>
2014-02-16 18:03:57 +0600

    blog

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
-  has_many :comments
+  has_many :comments, dependent: :destroy 
   validates :title, presence: true,
                     length: { minimum: 5 }
-  [...]
+  ['oleg']
 end
